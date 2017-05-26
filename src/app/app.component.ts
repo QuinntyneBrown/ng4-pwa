361ccc763dc08016fb8d5794b298f61b28d7e6bf
Quinntyne Brown <quinntynebrown@gmail.com>
2017-05-26 18:43:06 -0400

    initial code commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+
+@Component({
+    template: require("./app.component.html"),
+    styles: [require("./app.component.scss")],
+    selector: "ce-app"
+})
+export class AppComponent {    
+    constructor() { }
+    
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import { AppComponent } from "./app.component";
+
+@NgModule({
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        HttpModule
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+export * from "./environment";
